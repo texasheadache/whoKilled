b93f98967dd6175165a72bc12093f44ec8d2f479
texasheadache <jranck333@gmail.com>
2021-01-22 03:26:16 -0600

    adding main menu

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuButtons : MonoBehaviour
+{
+
+    public Button startButton;
+    public Button optionsButton;
+    public Button quitButton; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("LivingRoom");
+    }
+
+    public void quittingGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting");
+    }
+
+}
